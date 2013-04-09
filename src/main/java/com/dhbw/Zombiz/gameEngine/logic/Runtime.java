40e7a1c20ff43267518b2b8a0c65537a96ac6872
Chris <chrisii@gmx.de>
2013-04-09 22:39:20 +0200

    addet Runtime class with save function

---
+++ src/main/java/com/dhbw/Zombiz/gameEngine/logic/Runtime.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Copyright (c) 2013 DHBW.
+ * This source is subject to the DHBW Permissive License.
+ * Please see the License.txt file for more information.
+ * All other rights reserved.
+ * 
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ * 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ *Project: Zombiz
+ *Package: com.dhbw.Zombiz.gameEngine.logic
+ *
+ *Contributors:
+ * -Christoph Schabert
+
+ */
+package com.dhbw.Zombiz.gameEngine.logic;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class Runtime{
+	
+	private static String savegame = "src\\main\\resources\\savegame.sav";
+	
+	//Runtime Variables
+	
+	private static List <Actor> metActors = null;
+	private static List <Room> enterdRooms = null;
+	private static List <Room> enterableRooms = null;
+	private static int enterdRoomCounter = 0;
+	static int test = 42;
+	
+	/** Construtor for a new Game
+	 * 
+	 */
+	public Runtime(){
+	}
+
+	/** Construtor for load a Game
+	 * @param load = 1 [load Savegame]
+	 */
+	public Runtime(boolean load){
+	
+	}
+	
+	public static void saveGame(){
+		
+		try{
+			// Open a file to write to, named SavedObj.sav.
+			FileOutputStream saveFile=new FileOutputStream(savegame);
+	
+			// Create an ObjectOutputStream to put objects into save file.
+			ObjectOutputStream save = new ObjectOutputStream(saveFile);
+		
+			//save the Runtime Data
+			save.writeObject(metActors);
+			save.writeObject(enterdRooms);
+			save.writeObject(enterableRooms);
+			save.writeObject(enterdRoomCounter);
+			
+			// Close the file.
+			save.close();
+		}catch(Exception exc){
+			System.out.println("Unable to Save game");
+			exc.printStackTrace(); // If there was an error, print the info.
+		}
+	}
+
+	public static void loadGame(){
+	
+		try{
+			// Open file to read from, named SavedObj.sav.
+			FileInputStream saveFile = new FileInputStream(savegame);
+		
+			// Create an ObjectInputStream to get objects from save file.
+			ObjectInputStream save = new ObjectInputStream(saveFile);
+		
+			// Load the Objects
+			metActors = (List<Actor>) save.readObject();
+			enterdRooms = (List<Room>) save.readObject();
+			enterableRooms = (List<Room>) save.readObject();
+			enterdRoomCounter = (Integer) save.readObject();
+
+			// Close the file.
+			save.close();
+		}catch(Exception exc){
+			System.out.println("Unable to load Savegame");
+			exc.printStackTrace();
+		}
+	}
+}
